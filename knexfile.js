a0d34c688ec83f623bad4eadc33c47cbd136d4e4
Kelvin He <zhe2705@gmail.com>
2023-01-05 16:04:19 -0500

    fixing knexfile

--- knexfile.js
+++ knexfile.js
@@ -19,36 +19,16 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   test: {
