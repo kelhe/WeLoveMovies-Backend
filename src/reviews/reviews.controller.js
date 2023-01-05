df1be1993011e3a1cb5ceec8f606a1b67b31bf27
Kelvin He <zhe2705@gmail.com>
2023-01-05 13:58:40 -0500

    Fixed get /movies/:movieId/reviews endpoint to only show reviews for movies with matching id

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -10,9 +10,11 @@ organization_name: ["critic", "organization_name"]
 })
 
 
-async function list(req, res) {  
+async function list(req, res) {
+  const movie = res.locals.movie
   const data = await reviewsService.list();
-  res.json({data : reduceCritics(data)});
+  //res.json where the data returned has a conditional statement where if res.locals.movies exist from the movieExist middleware then we will filter and only return the reviews with matching movieId as params in the path
+  res.json({data : reduceCritics(data).filter(movie ? m => movie.movie_id == m.movie_id : () => true) }); 
 }
 
 async function reviewExists(req, res, next) {
